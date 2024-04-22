5443d2d5fbf150e9949bb15c4525b50f307535c7
Adam Suchi Hafizullah <adamsuchihafizullah@gmail.com>
2024-04-22 22:37:18 +0700

    update: contoh konversi tipe data

--- const.go
+++ const.go
@@ -6,6 +6,13 @@ func main() {
 	const firstName = "Eko"
 	const lastName string = "Khannedy"
 
+	const (
+		city    = "Muara Enim"
+		country = "Indonesia"
+	)
+
 	fmt.Println(firstName)
 	fmt.Println(lastName)
+	fmt.Println(city)
+	fmt.Println(country)
 }
